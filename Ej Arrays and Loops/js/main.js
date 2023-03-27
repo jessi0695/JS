8f380d4955508fdc40671d258e327d067f84ebec
jessica <jessi0685@gmail.com>
2023-03-27 16:46:09 -0600

    Se sube avance de ejercicio de arreglos y ciclos

---
+++ Ej Arrays and Loops/js/main.js
@@ -0,0 +1,16 @@
+//Part 1
+let nombres = ["Sofia","David","Juan",];
+console.log(nombres);
+
+//Part 2
+nombres.push("Sara", "Augustin");
+console.log(nombres);
+let firstElement = nombres.shift();
+console.log(nombres);
+
+//Part 3
+nombres.push("Elena");
+console.log(nombres);
+console.log(nombres.unshift(Renata));
+
+//Ejercicio 2
